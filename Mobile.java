96311a34dbd9e4ab740e7ce8b8dccae3f5ee18f7
Linga10 <121711460+Linga10@users.noreply.github.com>
2022-12-31 13:41:50 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Mobile {
+	
+	public void message()
+	{
+		System.out.println("Linga");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Mobile m = new Mobile();
+m.message();
+
+	}
+
+}
